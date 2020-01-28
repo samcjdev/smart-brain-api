e9dc11feaff32b6328cb83e3d0a075a9dcd2ee78
cj69 <cj69@users.noreply.github.com>
2020-01-28 23:48:17 +0000

    changing repsonse

--- server.js
+++ server.js
@@ -19,14 +19,12 @@ const db = knex({
     }
   });
 
-
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => { res.send(database.users) })
+app.get("/", (req, res) => { res.send("it is working") })
 app.post("/signin", (req, res) => { signin.handleSignin (db, bcrypt)(req, res) })
 app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) })
