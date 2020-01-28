f2910f1fd7f21a818407475912fc2c387b7bb490
cj69 <cj69@users.noreply.github.com>
2020-01-28 22:17:18 +0000

    adding port

--- server.js
+++ server.js
@@ -39,11 +39,10 @@ app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) })
 // Load hash from your password DB.
 
 
-app.listen(3000, () => {
-    console.log("app is runnning on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.port}`);
 });
 
-console.log(process.env)
 /*
 / --> res = this is working
 /signin --> POST  success/fail
