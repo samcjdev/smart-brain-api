66e08010ebac3fe6bcf9aecd7ef2357d54baeaaf
cj69 <cj69@users.noreply.github.com>
2020-01-29 12:09:06 +0000

    update of db inline with heroku connecting to node docs

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-cubic-25550',
-      user : 'postgres',
-      password : '90gsm',
-      database : 'smart-brain'
+      host : 'process.env.DATABASE_URL',
+      ssl: true
     }
   });
 
